4b72fe65089086cdd275b11846862e6012c17f42
Pavel Naboka <Pavmn96@yandex.ru>
2025-04-27 21:49:42 +0300

    Initial commit
    Create empty Spring Application

---
+++ src/test/java/edu/kata/spring_boot_security/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kata.spring_boot_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
